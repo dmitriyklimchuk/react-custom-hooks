7a9cf5d73f4cdd36a264d3f21f234f37e6ed94b4
dmytroklymchuk <dmytro.klymchuk@valtech.com>
2023-10-04 17:06:27 +0300

    Add custom hook in more components

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,49 +1,35 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+    const {isLoading, error, sendRequest: sendTaskRequest} = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-23698-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
+    const enterTaskHandler = async (taskText) => {
+
+        const createTask = (taskData) => {
+            const generatedId = taskData.name;
+            const createdTask = {id: generatedId, text: taskText};
+
+            props.onAddTask(createdTask);
         }
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+        sendTaskRequest({
+            url: 'https://react-http-23698-default-rtdb.firebaseio.com/tasks.json',
+            method: 'POST',
+            body: {text: taskText},
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }, createTask)
+    };
 
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  return (
-    <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
-      {error && <p>{error}</p>}
-    </Section>
-  );
+    return (
+        <Section>
+            <TaskForm onEnterTask={enterTaskHandler} loading={isLoading}/>
+            {error && <p>{error}</p>}
+        </Section>
+    );
 };
 
 export default NewTask;
